f1360f337871ea18988dc8849d33021e20fc0a36
NISCHAY SAI D R <nischaysai35@gmail.com>
2025-02-19 10:03:35 +0530

    changes

---
+++ practice_code.py
@@ -0,0 +1,25 @@
+
+n=int(input("Enter no. of oranges : "))
+l1=list(map(int,input("Enter size of oranges : ").split()))
+print(l1)
+l2=[]
+l3=[]
+l4=[]
+l5=[]
+
+k=l1[n-1]
+print('k=',k)
+for i in l1:
+    if i<k:
+        l2.append(i)
+        print("appended i",i)
+    elif i==k:
+        l3.append(i)
+        print("appended i",i)
+    else:
+        l4.append(i)
+        print("appended i",i)
+    print(f'l2={l2},l3={l3},l4={l4}')
+print(l2,l3,l4,l5)
+l5=l2+l3+l4
+print(l5)
